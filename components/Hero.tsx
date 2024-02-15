da613ffec424909362c9ee6f8b7cff718f150bb5
Micka <mickael.murmann@gmail.com>
2024-02-15 17:03:01 +0100

    update Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="max-container padding-container flex flex-col gap-20">
+
+    </section>
   )
 }
 
